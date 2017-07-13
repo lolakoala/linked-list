1a0263426c8021b522b83cd4e23a5e5f67bbd273
Luke Finney <lucaswfinney@gmail.com>
2017-07-13 08:37:33 -0600

    Final commit

--- scripts.js
+++ scripts.js
@@ -2,11 +2,10 @@ var enterButton = document.querySelector('.enter-button');
 var titleInput = document.querySelector('#title-input');
 var urlInput = document.querySelector('#url-input');
 
-titleInput.addEventListener('input', enableEnter);
-urlInput.addEventListener('input', enableEnter);
-
 function enableEnter() {
-  if ((titleInput.value !== "") && (urlInput.value !== "")) {
+  var userTitle = titleInput.value;
+  var userUrl = urlInput.value;
+  if ((userTitle !== "") && (userUrl !== "")) {
     enterButton.removeAttribute("disabled");
   } else {
     enterButton.setAttribute("disabled");
@@ -15,74 +14,76 @@ function enableEnter() {
 
 function makeCard(event) {
   event.preventDefault();
-
   var title = titleInput.value;
   var url = urlInput.value;
-
-   var newBookmark = createBookmark(title,url);
-   var bookmarkSection = document.querySelector('.gray-box');
-   if ((title === "") || (url === "")){
-     alert("You must have both a title and a url to create a bookmark!");
-   } else {bookmarkSection.appendChild(newBookmark);
-   };
+  var newBookmark = createBookmark(title, url);
+  var bookmarkSection = document.querySelector('.gray-box');
+  if (title === "") {
+   titleInput.placeholder = "Please enter a title";
+  } else if (url === "") {
+   urlInput.placeholder = "Please enter a url";
+  }
+  else {bookmarkSection.appendChild(newBookmark);
+  };
   cardCounter();
-
+  inputReset();
  };
 
-function createBookmark(title,url) {
+function createBookmark(title, url) {
   var newDiv = document.createElement('div');
   newDiv.classList.add('bookmark-card');
-  newDiv.innerHTML = `<h2>${title}</h2>
-  <span class="bookmark-link">
+  newDiv.innerHTML = `
+    <h2>${title}</h2>
+    <span class="bookmark-link">
       <a href="${url}" class="bookmark-url" target="_blank">${url}</a>
-  </span>
-  <div class="button-flex-box">
-    <button class="bookmark-read-button">Read</button>
-    <button class="bookmark-delete-button">Delete</button>`;
-
-    deleteButton = newDiv.querySelector('.bookmark-delete-button');
-
-
+    </span>
+    <div class="button-flex-box">
+      <button class="bookmark-read-button">Read</button>
+      <button class="bookmark-delete-button">Delete</button>
+    </div>`;
+  deleteButton = newDiv.querySelector('.bookmark-delete-button');
   deleteButton.addEventListener('click', function() {
-    this.closest('.bookmark-card').remove();
-    cardCounter();
+  this.closest('.bookmark-card').remove();
+  cardCounter();
+  readCardCount();
   });
-
-
-
   return newDiv;
-
 };
 
-$('.gray-box').on('click', '.bookmark-read-button', function () {
-      $(this).closest('.bookmark-card').toggleClass('read');
-      $(this).parents('.bookmark-card').find('.bookmark-url').toggleClass('read');
-      $(this).parents('.bookmark-card').find('.bookmark-read-button').toggleClass('read');
-      // console.log('readButton')
-    });
-
 function cardCounter() {
-  var cardCount = document.querySelectorAll('.gray-box .bookmark-card').length;
-    console.log('Number of bookmark cards is: ', cardCount);
-  var totalBookmarks = document. getElementById('total-bookmarks');
-      totalBookmarks.innerText = cardCount
+  var cardCount = document.querySelectorAll('.bookmark-card').length;
+  var readCount = document.querySelectorAll('button.read').length;
+  var unreadCount = cardCount - (readCount/2);
+  var totalBookmarks = document.getElementById('total-bookmarks');
+  var unreadBookmarks = document.getElementById('unread-bookmarks');
+  totalBookmarks.innerText = cardCount;
+  unreadBookmarks.innerText = unreadCount;
+};
 
-  var readCount = document.querySelectorAll('.gray-box .bookmark-card button.read').length;
-  console.log('Number of read bookmark is: ', readCount);
+function readCardCount() {
+  var cardCount = document.querySelectorAll('.bookmark-card').length;
+  var readCount = document.querySelectorAll('button.read').length;
+  var unreadCount = cardCount - (readCount/2);
+  var readBookmarks = document.getElementById('read-bookmarks');
+  var unreadBookmarks = document.getElementById('unread-bookmarks');
+  readBookmarks.innerText = (readCount/2);
+  unreadBookmarks.innerText = unreadCount;
   };
 
-//pseudo code what needs to happen for 2.75
-  // target book card with '.read' class active
-    // count number of active .read cards
-    // count should update on click of "readButton"
-      // return number of active .read cards to DOM into #read-bookmarks <p>
-        // update live as cards are toggled read/not read
-  // target book card with '.read' class inactive
-    // count number of inactive .read cards
-    //count should update on click of "readButton"
-      // return number of inactive .read cards to DOM into #read-bookmarks <p>
-        // update live as cards are toggled read/not read
-  // Possible test: read cards + unread cards === total cards
+  $('.gray-box').on('click', '.bookmark-read-button', function () {
+        $(this).closest('.bookmark-card').toggleClass('read');
+        $(this).parents('.bookmark-card').find('.bookmark-url').toggleClass('read');
+        $(this).parents('.bookmark-card').find('.bookmark-read-button').toggleClass('read');
+        $(this).parents('.bookmark-card').find('.bookmark-delete-button').toggleClass('read');
+        readCardCount();
+      });
 
+function inputReset() {
+  enterButton.disabled = true;
+  titleInput.value = "";
+  urlInput.value = "";
+};
 
 enterButton.addEventListener('click', makeCard);
+titleInput.addEventListener('input', enableEnter);
+urlInput.addEventListener('input', enableEnter);
